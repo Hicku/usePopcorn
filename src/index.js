939b8eb1578cb02c1fd96e2b5d14fce9532c9cf7
unknown <aaron.hickman201@gmail.com>
2024-02-17 14:12:14 +0000

    star rating

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StarRating";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={2} />
   </React.StrictMode>,
   document.getElementById("root")
 );
