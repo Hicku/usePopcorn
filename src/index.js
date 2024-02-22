e7cfd1b82dcc16c923af6a1d3fc8300ac94bca48
unknown <aaron.hickman201@gmail.com>
2024-02-22 14:20:24 +0000

    useEffect

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import "./index.css";
-// import App from "./App";
+import App from "./App-v2";
 import StarRating from "./StarRating";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
-    <StarRating size={24} color="red" className="test" defaultRating={2} />
+    <StarRating size={24} color="red" className="test" defaultRating={2} /> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
